c2edf8c10034cf9096ecafcc40b094a3dbf001e0
Demian Wassermann <demian@bwh.harvard.edu>
2012-07-17 19:21:31 -0400

    Outliser rejection

--- tract_querier/__init__.py
+++ tract_querier/__init__.py
@@ -1,1 +1,2 @@
 from query_processor import *
+from bundle_outlier_treatment import *
--- tract_querier/bundle_outlier_treatment.py
+++ tract_querier/bundle_outlier_treatment.py
@@ -1,5 +1,78 @@
 import numpy as np
 from scipy import ndimage
+import ipdb
+
+def hausdorff_distance(fiber1, fiber2, **kwargs):
+    return np.sqrt(((fiber1[None,...] - fiber2[:, None, ...]) ** 2).sum(-1).max())
+
+def wassermann_inner_product(fiber1, fiber2, sigma1=4, sigma2=4, tapering=np.inf):
+    sqr_distance = ((fiber1[None,...] - fiber2[:, None, ...]) ** 2).sum(-1)
+
+    s1_p_s2 = sigma1 + sigma2
+    s1s2 = sigma1 * sigma2
+
+    #Tapering the matrix at 3 times the minimum sigma
+    exp_term = np.exp( - .5 *  sqr_distance / s1_p_s2) * (sqr_distance < tapering * min(sigma1, sigma2))
+
+    sim = (s1s2 / np.sqrt(s1_p_s2) * exp_term).sum()
+
+    return np.sqrt(2) * sim
+
+def inner_product_matrix(fibers, inner_product_function=wassermann_inner_product, **kwargs):
+    distance_matrix = np.zeros((len(fibers), len(fibers)))
+    for i in xrange(len(fibers)):
+        for j in xrange(i + 1):
+            distance_matrix[i, j] = inner_product_function(fibers[i], fibers[j], **kwargs)
+            distance_matrix[j, i] = distance_matrix[i, j]
+    return distance_matrix
+
+
+def bundle_distances(fibers, inner_product=wassermann_inner_product, **kwargs):
+    distance_matrix = inner_product_matrix(inner_product, kwargs, fibers)
+
+    squared_norms = np.diag(distance_matrix)
+
+    distance_matrix *= -2
+    distance_matrix += squared_norms[:, None]
+    distance_matrix += squared_norms[None, :]
+
+    return distance_matrix ** .5
+
+
+def bundle_normed_distances(fibers, inner_product=wassermann_inner_product, **kwargs):
+    distance_matrix = inner_product_matrix(inner_product, kwargs, fibers)
+
+    norms = np.sqrt(np.diag(distance_matrix))
+
+    distance_matrix /= norms[:, None]
+    distance_matrix /= norms[None, :]
+
+    distance_matrix *= -2
+    distance_matrix += 2
+
+    return distance_matrix.round(6) ** .5
+
+def z_score_outlier_rejection(fibers, z_score_reject=4, **kwargs):
+    inner_products = inner_product_matrix(fibers, **kwargs)
+
+    inner_products_to_whole_bundle = inner_products.mean(1)
+
+    whole_bundle_squared_norm = inner_products_to_whole_bundle.sum()
+
+    distances_to_whole_bundle = whole_bundle_squared_norm + inner_products.diagonal() - 2 * inner_products_to_whole_bundle
+
+    #Estimation of the gamma function parameters
+    s = np.log(distances_to_whole_bundle.mean()) - np.log(distances_to_whole_bundle).mean()
+    k = (3 - s + np.sqrt((s-3) ** 2 + 24 * s) ) / (12 * s)
+    theta = distances_to_whole_bundle.mean() / k
+
+    mean = k * theta
+    std = np.sqrt(k) * theta
+
+    z_score = (distances_to_whole_bundle - mean) / std
+
+    return np.where(np.abs(z_score) < z_score_reject)[0], np.abs(z_score)
+
 
 def mean_density_map(fibers, sigma=4, resolution=4):
     points = np.vstack(fibers)
@@ -31,7 +104,7 @@ def bundle_z_scores(fibers, sigma=4, resolution=4):
     fibers_ijk = []
     for i,f in enumerate(fibers):
         im[:] = 0
-        fibers_ijk.append(((f - points_min)/resolution).round().astype(int)) 
+        fibers_ijk.append(((f - points_min)/resolution).round().astype(int))
         im[ tuple(fibers_ijk[-1].T)] = 1
         ndimage.gaussian_filter(im, sigma_, output=im)
         mean_im += im
