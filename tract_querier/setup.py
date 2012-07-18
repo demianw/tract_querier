cd81402548e504cb5b0878bd2295620c78590428
Demian Wassermann <demian@bwh.harvard.edu>
2012-07-18 10:51:01 -0400

    Added tensor covariance

--- tract_querier/setup.py
+++ tract_querier/setup.py
@@ -3,9 +3,10 @@
 def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('tract_querier', parent_package, top_path)
+    config.add_subpackage('tensor_covariance')
     return config
 
 if __name__ == '__main__':
     from distutils.core import setup
     setup(**configuration(top_path='').todict())
-         
+
