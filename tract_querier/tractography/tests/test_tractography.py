b51422d6432bac768f481fc8529ec1a9b8b6bc74
Demian Wassermann <demian@bwh.harvard.edu>
2013-05-24 07:16:02 -0400

    ENH: Added testing to tractography

--- tract_querier/tractography/tests/test_tractography.py
+++ tract_querier/tractography/tests/test_tractography.py
@@ -4,38 +4,65 @@ from nose.tools import with_setup
 import copy
 from itertools import izip, chain
 
+from numpy import any, all
 from numpy.random import randint, randn
 
 dimensions = None
 tracts = None
-tract_data = None
+tracts_data = None
 tractography = None
 max_tract_length = 50
 n_tracts = 50
 
 
+def equal_tracts(a, b):
+    for t1, t2 in izip(a, b):
+        if not all(t1 == t2):
+            return False
+
+    return True
+
+
+def equal_tracts_data(a, b):
+    if set(a.keys()) != set(b.keys()):
+        return False
+
+    for k in a.keys():
+        for t1, t2 in izip(a[k], b[k]):
+            if not all(t1 == t2):
+                return False
+    return True
+
+
+def equal_tractography(a, b):
+    return (
+        equal_tracts(a.tracts(), b.tracts()) and
+        equal_tracts_data(a.tracts_data(), b.tracts_data())
+    )
+
+
 def setup(*args, **kwargs):
     global dimensions
     global tracts
-    global tract_data
+    global tracts_data
     global tractography
 
     dimensions = [(randint(5, max_tract_length), 3) for _ in xrange(n_tracts)]
     tracts = [randn(*d) for d in dimensions]
-    tract_data = {
+    tracts_data = {
         'a%d' % i: [
             randn(d[0], k)
             for d in dimensions
         ]
         for i, k in zip(xrange(4), randint(1, 5, 4))
     }
-    tractography = Tractography(tracts, tract_data)
+    tractography = Tractography(tracts, tracts_data)
 
 
 @with_setup(setup)
 def test_creation():
-    assert(tractography.tracts() is tracts and tractography.original_tracts() is tracts)
-    assert(tractography.tracts_data() is tract_data and tractography.original_tracts_data() is tract_data)
+    assert(equal_tracts(tractography.tracts(), tracts))
+    assert(equal_tracts_data(tractography.tracts_data(), tracts_data))
     assert(not tractography.are_tracts_subsampled())
     assert(not tractography.are_tracts_filtered())
 
@@ -51,27 +78,28 @@ def test_subsample_tracts():
             for value in tractography.tracts_data().values()
         )
     )
-    assert(tractography.tracts() is not tracts and tractography.original_tracts() is tracts)
-    assert(tractography.tracts_data() is not tract_data and tractography.original_tracts_data() is tract_data)
+
+    assert(not equal_tracts(tractography.tracts(), tractography.original_tracts()))
+    assert(equal_tracts(tracts, tractography.original_tracts()))
+    assert(not equal_tracts_data(tractography.tracts_data(), tractography.original_tracts_data()))
+    assert(equal_tracts_data(tracts_data, tractography.original_tracts_data()))
     assert(tractography.are_tracts_subsampled())
 
     tractography.unsubsample_tracts()
-    assert(tractography.tracts() is tracts and tractography.original_tracts() is tracts)
-    assert(tractography.tracts_data() is tract_data and tractography.original_tracts_data() is tract_data)
+    assert(equal_tracts(tractography.tracts(), tracts))
+    assert(equal_tracts_data(tractography.tracts_data(), tracts_data))
     assert(not tractography.are_tracts_subsampled())
+    assert(not tractography.are_tracts_filtered())
 
 
 @with_setup(setup)
 def test_append():
     old_tracts = copy.deepcopy(tractography.tracts())
     new_data = {}
-    for k, v in tract_data.iteritems():
+    for k, v in tracts_data.iteritems():
         new_data[k] = v + v
 
-    tractography.append(tracts, tract_data)
-    for t1, t2 in izip(tractography.tracts(), chain(old_tracts, old_tracts)):
-        assert((t1 == t2).all())
+    tractography.append(tracts, tracts_data)
 
-    for k in tractography.tracts_data():
-        for t1, t2 in izip(tractography.tracts_data()[k], new_data[k]):
-            assert((t1 == t2).all())
+    assert(equal_tracts(tractography.tracts(), chain(old_tracts, old_tracts)))
+    assert(equal_tracts_data(tractography.tracts_data(), new_data))
