a7b5a0eaab033d0f3015687d8897c6eba2f82dca
Demian Wassermann <demian@bwh.harvard.edu>
2013-07-05 08:06:36 -0400

    ENH: Corrected path to the main queries for different installation scenarios

--- tract_querier/__init__.py
+++ tract_querier/__init__.py
@@ -1,7 +1,17 @@
+import os
+
 from query_processor import *
 from tract_label_indices import *
 from shell import *
 import tractography
+
+
+default_queries_folder = os.path.abspath(os.path.join(
+    os.path.dirname(__file__),
+    '..', '..', '..', '..',
+    'tract_querier', 'queries'
+))
+
 #import tract_metrics
 
 __version__ = 0.1
