bac740f012111c0d16a3d02104029cdefb72ac9d
Demian Wassermann <demian.wassermann@inria.fr>
2014-04-30 11:43:42 +0200

    ENH: Added first tests for scripts

---
+++ tract_querier/tests/datasets.py
@@ -0,0 +1,58 @@
+import hashlib
+import os
+from os import path
+import tempfile
+import urllib2
+
+FILES = {
+    'tract_file': (
+        'http://midas.kitware.com/bitstream/view/17631',
+        'IIT3mean_left_hemisphere_small.trk',
+        '\xe7\xec\xfd+\xd2n\xff\x96\xae\xb4\xdf+\x194\xdf\x81'
+    ),
+    'atlas_file': (
+        'http://midas.kitware.com/bitstream/view/17622',
+        'IIT3mean_desikan_2009.nii.gz',
+        'vx\x13\xbaE\x1dR\t\xcd\xc9EF\x17\xa66\xb7'
+    ),
+    'query_uf_file': (
+        'http://midas.kitware.com/bitstream/view/17627',
+        'wmql_2_uf.qry',
+        '\\+R\x8c<B#\xea\xfc\x9aE\xbd\xb0(\xbdn'
+    )
+}
+
+class TestDataSet(object):
+    def __init__(self):
+        self.dirname = path.join(
+            tempfile.gettempdir(),
+            'tract_querier'
+        )
+
+        self.files = {}
+
+        if not path.exists(self.dirname):
+            os.mkdir(self.dirname)
+
+        for k, v in FILES.iteritems():
+            dst_filename = path.join(self.dirname, v[1])
+
+            if (
+                not path.exists(dst_filename) or
+                hashlib.md5(open(dst_filename).read()).digest() != v[2]
+            ):
+                dl_file = urllib2.urlopen(v[0])
+                dst_file = open(dst_filename, 'wb')
+                dst_file.write(dl_file.read())
+                dst_file.close()
+
+            if (
+                hashlib.md5(open(dst_filename).read()).digest() != v[2]
+            ):
+                raise IOError('File %s url %s was not properly downloaded' % (v[1], v[0]))
+
+            self.files[k] = dst_filename
+
+
+
+
---
+++ tract_querier/tests/test_scripts.py
@@ -0,0 +1,81 @@
+from nose.tools import assert_equal, assert_greater, assert_in, assert_is_not_none, assert_true
+
+import os
+from os import path
+import re
+import subprocess
+import sys
+
+from tract_querier.tests import datasets
+
+
+PACKAGE_ROOT_DIR = path.dirname(path.dirname(path.dirname(__file__)))
+
+TRACT_QUERIER_SCRIPT = path.join(
+    PACKAGE_ROOT_DIR,
+    'scripts', 'tract_querier'
+)
+
+TRACT_MATH_SCRIPT = path.join(
+    PACKAGE_ROOT_DIR,
+    'scripts', 'tract_math'
+)
+
+PYTHON = sys.executable
+
+ENVIRON = os.environ.copy()
+sys.path.insert(0, PACKAGE_ROOT_DIR)
+ENVIRON['PYTHONPATH'] = reduce(lambda x, y: '%s:%s' % (x, y), sys.path)
+
+TEST_DATA = datasets.TestDataSet()
+
+def test_tract_querier_help():
+    popen = subprocess.Popen(
+        [PYTHON, TRACT_QUERIER_SCRIPT],
+        shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+        env=ENVIRON
+    )
+    popen.wait()
+    stderr_text = ''.join(popen.stderr.readlines())
+    assert_in('error: incorrect number of arguments', stderr_text)
+    assert_greater(popen.returncode, 0)
+
+def test_tract_math_help():
+    popen = subprocess.Popen(
+        [PYTHON, TRACT_MATH_SCRIPT],
+        shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+        env=ENVIRON
+    )
+    popen.wait()
+    stderr_text = ''.join(popen.stderr.readlines())
+    assert_in('error: too few arguments', stderr_text)
+    assert_greater(popen.returncode, 0)
+
+def test_tract_math_count():
+    popen = subprocess.Popen(
+        [PYTHON, TRACT_MATH_SCRIPT, TEST_DATA.files['tract_file'], 'count'],
+        shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+        env=ENVIRON
+    )
+    popen.wait()
+    stdout_text = ''.join(popen.stdout.readlines())
+    assert_is_not_none(re.search('[^0-9]6783[^0-9]', stdout_text))
+    assert_equal(popen.returncode, 0)
+
+def test_tract_querier_query():
+    output_prefix = '%s/test' % TEST_DATA.dirname
+    popen = subprocess.Popen(
+        [PYTHON, TRACT_QUERIER_SCRIPT] +
+        ('-a %(atlas_file)s -t %(tract_file)s -q %(query_uf_file)s' % TEST_DATA.files).split() +
+        (' -o %s' % output_prefix).split(),
+        shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+        env=ENVIRON
+    )
+    popen.wait()
+    stdout_text = ''.join(popen.stdout.readlines())
+    assert_in('uncinate.left: 000102', stdout_text)
+    assert_in('uncinate.right: 000000', stdout_text)
+    assert_true(path.exists(output_prefix + '_uncinate.left.trk'))
+    assert_equal(popen.returncode, 0)
+    if path.exists(output_prefix + '_uncinate.left.trk'):
+        os.remove(output_prefix + '_uncinate.left.trk')
