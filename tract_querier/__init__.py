04c840bca7a4918dc00fa7e8e6e3eee4e1ad04d4
Jean-Christophe Houde <jean.christophe.houde@gmail.com>
2014-07-08 15:29:47 -0400

    Modified way to get the default queries path.

--- tract_querier/__init__.py
+++ tract_querier/__init__.py
@@ -1,4 +1,5 @@
 import os
+import sysconfig
 
 from query_processor import *
 from tract_label_indices import *
@@ -6,11 +7,25 @@ from shell import *
 import tractography
 
 
-default_queries_folder = os.path.abspath(os.path.join(
-    os.path.dirname(__file__),
-    '..', '..', '..', '..',
-    'tract_querier', 'queries'
-))
+def find_queries_path():
+  	default_data_path = sysconfig.get_path(name='data')
+  	queries_path = os.path.join(default_data_path, 'tract_querier', 'queries')
+
+  	if not os.path.exists(queries_path):
+    		# Try to manage Virtual Environments on some OSes,
+    		# where data is not put the 'local' subdirectory,
+    		# but at the root of the virtual environment.
+    		if default_data_path.endswith('local'):
+    		    queries_path = os.path.join(default_data_path.rsplit('local', 1)[0],
+    						                        'tract_querier', 'queries')
+
+    			  if not os.path.exists(queries_path):
+    				    raise Exception('Default path for queries not found')
+
+    return queries_path
+
+
+default_queries_folder = find_queries_path()
 
 #import tract_metrics
 
