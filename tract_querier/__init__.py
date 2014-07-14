bd8758c08fc722643193393dffcdfa4f60ec95aa
Jean-Christophe Houde <jean.christophe.houde@gmail.com>
2014-07-14 09:36:37 -0400

    Added path for git cloned setup, changed way paths are searched.

--- tract_querier/__init__.py
+++ tract_querier/__init__.py
@@ -8,21 +8,27 @@ import tractography
 
 
 def find_queries_path():
-    default_data_path = sysconfig.get_path(name='data')
-    queries_path = os.path.join(default_data_path, 'tract_querier', 'queries')
+    default_path = sysconfig.get_path(name='data')
+    possible_paths = [os.path.join(default_path, 'tract_querier', 'queries')]
 
-    if not os.path.exists(queries_path):
-        # Try to manage Virtual Environments on some OSes,
-        # where data is not put the 'local' subdirectory,
-        # but at the root of the virtual environment.
-        if default_data_path.endswith('local'):
-            queries_path = os.path.join(default_data_path.rsplit('local', 1)[0],
-                                        'tract_querier', 'queries')
+    # Try to manage Virtual Environments on some OSes,
+    # where data is not put the 'local' subdirectory,
+    # but at the root of the virtual environment.
+    if default_path.endswith('local'):
+        possible_paths.append(os.path.join(default_path.rsplit('local', 1)[0],
+                                           'tract_querier', 'queries'))
 
-            if not os.path.exists(queries_path):
-                raise Exception('Default path for queries not found')
+    # Case where the Tract_querier is cloned from git and simply
+    # added to the python path, without installation.
+    possible_paths.append(os.path.abspath(os.path.join(
+                                          os.path.dirname(__file__), 'data')))
 
-    return queries_path
+    paths_found = [path for path in possible_paths if os.path.exists(path)]
+
+    if not paths_found:
+        raise Exception('Default path for queries not found')
+
+    return paths_found[0]
 
 
 default_queries_folder = find_queries_path()
