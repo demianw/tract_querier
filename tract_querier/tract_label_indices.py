6227d12241932afe285b748f73955041bfe511f5
Demian Wassermann <demian.wassermann@inria.fr>
2016-11-03 13:17:12 +0100

    Cleaned module, improved py3k compatibility

--- tract_querier/tract_label_indices.py
+++ tract_querier/tract_label_indices.py
@@ -1,4 +1,7 @@
 import warnings
+
+from six.moves import range
+
 import numpy as np
 
 from .aabb import BoundingBox
@@ -23,7 +26,8 @@ class TractographySpatialIndexing:
     length_threshold : float
         minimum length in mm of a tract to be considered in the indexing
     crossing_threshold : float
-        the ratio of a tract that needs to be inside a label to be considered that it crosses it
+        the ratio of a tract that needs to be inside a label to be considered
+        that it crosses it
 
     Attributes
     ----------
@@ -36,7 +40,8 @@ class TractographySpatialIndexing:
     length_threshold : float
         minimum length in mm of a tract to be considered in the indexing
     crossing_threshold : float
-        the ratio of a tract that needs to be inside a label to be considered that it crosses it
+        the ratio of a tract that needs to be inside a label to be considered
+        that it crosses it
     crossing_tracts_labels : dict of sets
         Dictionary indexed by tract number of the labels traversed by the tract
     crossing_labels_tracts : dict of sets
@@ -47,11 +52,15 @@ class TractographySpatialIndexing:
     ending_labels_tracts : (dict of sets, dict of sets)
         Dictionary of each endpoint of the tracts indexed by label number and
         containing the tracts at which the endpoint in the label is
-    tract_endpoints_pos : array_like of :math:`N\times 2 \times 3` where :math:`N` is the number of tracts
+    tract_endpoints_pos : array_like of :math:`N\times 2 \times 3` where
+        :math:`N` is the number of tracts
         Contains the position of both endpoints of each tract
     """
 
-    def __init__(self, tractography, image, affine_ijk_2_ras,  length_threshold, crossing_threshold):
+    def __init__(
+        self, tractography, image, affine_ijk_2_ras,
+        length_threshold, crossing_threshold
+    ):
         self.tractography = tractography
         self.image = image
         self.affine_ijk_2_ras = affine_ijk_2_ras
@@ -67,8 +76,12 @@ class TractographySpatialIndexing:
             self.tractography, self.length_threshold, self.crossing_threshold
         )
 
-        self.label_bounding_boxes = compute_label_bounding_boxes(self.image.astype(int), self.affine_ijk_2_ras)
-        self.tract_bounding_boxes = compute_tract_bounding_boxes(self.tractography)
+        self.label_bounding_boxes = compute_label_bounding_boxes(
+            self.image.astype(int), self.affine_ijk_2_ras
+        )
+        self.tract_bounding_boxes = compute_tract_bounding_boxes(
+            self.tractography
+        )
 
         self.tract_endpoints_pos = np.empty((len(self.tractography), 2, 3))
 
@@ -155,7 +168,7 @@ def compute_tract_bounding_boxes(tracts, affine_transform=None):
 
 def compute_label_crossings(tract_cumulative_lengths, point_labels, threshold):
     tracts_labels = {}
-    for i in xrange(len(tract_cumulative_lengths) - 1):
+    for i in range(len(tract_cumulative_lengths) - 1):
         start = tract_cumulative_lengths[i]
         end = tract_cumulative_lengths[i + 1]
         label_crossings = np.asanyarray(point_labels[start:end], dtype=int)
@@ -175,7 +188,7 @@ def compute_label_crossings(tract_cumulative_lengths, point_labels, threshold):
 
 def compute_label_endings(tract_cumulative_lengths, point_labels):
     tracts_labels = {}
-    for i in xrange(len(tract_cumulative_lengths) - 1):
+    for i in range(len(tract_cumulative_lengths) - 1):
         start = tract_cumulative_lengths[i]
         end = tract_cumulative_lengths[i + 1]
         tracts_labels[i] = set((int(point_labels[
@@ -194,7 +207,7 @@ def compute_label_endings(tract_cumulative_lengths, point_labels):
 def compute_label_endings_start_end(tract_cumulative_lengths, point_labels):
     tracts_labels_start = {}
     tracts_labels_end = {}
-    for i in xrange(len(tract_cumulative_lengths) - 1):
+    for i in range(len(tract_cumulative_lengths) - 1):
         start = tract_cumulative_lengths[i]
         end = tract_cumulative_lengths[i + 1]
         tracts_labels_start[i] = int(point_labels[start])
@@ -212,7 +225,10 @@ def compute_label_endings_start_end(tract_cumulative_lengths, point_labels):
                 labels_tracts[l].add(i)
             else:
                 labels_tracts[l] = set((i,))
-    return (tracts_labels_start, tracts_labels_end), (labels_tracts_start, labels_tracts_end)
+    return (
+        (tracts_labels_start, tracts_labels_end),
+        (labels_tracts_start, labels_tracts_end)
+    )
 
 
 def compute_tract_label_indices(
@@ -220,8 +236,9 @@ def compute_tract_label_indices(
     tracts, length_threshold, crossing_threshold
 ):
     if length_threshold > 0:
-        tract_length = lambda tract: ((((tract[
-                                      1:] - tract[:-1]) ** 2).sum(1)) ** .5).sum()
+        tract_length = lambda tract: ((((
+            tract[1:] - tract[:-1]
+        ) ** 2).sum(1)) ** .5).sum()
         tracts = [f for f in tracts if tract_length(f) >= length_threshold]
 
     all_points = np.vstack(tracts)
@@ -229,13 +246,12 @@ def compute_tract_label_indices(
                       affine_ras_2_ijk[:-1, -1])
     all_points_ijk_rounded = np.round(all_points_ijk).astype(int)
 
-    if (
-        any(((all_points_ijk_rounded[:, i] >= img.shape[i]).any() for i in xrange(3))) or
-        (all_points_ijk_rounded < 0).any()
-    ):
+    if (any((
+        (all_points_ijk_rounded[:, i] >= img.shape[i]).any() for i in range(3))
+    ) or (all_points_ijk_rounded < 0).any()):
         warnings.warn("Warning tract points fall outside the image")
 
-    for i in xrange(3):
+    for i in range(3):
         all_points_ijk_rounded[:, i] = all_points_ijk_rounded[
             :,
             i
@@ -248,9 +264,8 @@ def compute_tract_label_indices(
         tract_cumulative_lengths, point_labels, crossing_threshold
     )
 
-    ending_tracts_labels, ending_labels_tracts = compute_label_endings_start_end(
-        tract_cumulative_lengths, point_labels
-    )
+    ending_tracts_labels, ending_labels_tracts = \
+        compute_label_endings_start_end(tract_cumulative_lengths, point_labels)
 
     return (
         crossing_tracts_labels,
@@ -258,123 +273,3 @@ def compute_tract_label_indices(
         ending_tracts_labels,
         ending_labels_tracts
     )
-
-
-# import collections
-# BinaryTreeNode = collections.namedtuple('key value left right')
-
-# class BinaryTreeDict:
-#    go_left = 1
-#    go_right = 2
-#    def __init__(self):
-#        self.root = None
-
-#    def __setitem__(self, key, value):
-#        new_node = BinaryTreeNode(key, value, None, None)
-
-#        if self.root is None:
-#            self.root = new_node
-#        else:
-#            current_node = self.root
-#            while True:
-#                node_key = current_node.key
-#                if key == node_key:
-#                    current_node.value = value
-#                    return
-#                elif key < node_key:
-#                    selected_child_attr = 'left'
-#                elif key > node_key:
-#                    selected_child_attr = 'right'
-
-#                selected_child = getattr(
-#                    current_node,
-#                    selected_child_attr
-#                )
-
-#                if selected_child is not None:
-#                    current_node = selected_child
-#                else:
-#                    break
-
-#            setattr(current_node, selected_child_attr, new_node)
-
-#    def update_item(self, key, value):
-#        new_node = BinaryTreeNode(key, value, None, None)
-
-#        if self.root is None:
-#            self.root = new_node
-#        else:
-#            current_node = self.root
-#            while True:
-#                node_key = current_node.key
-#                if key == node_key:
-#                    current_node.value.update(value)
-#                    return
-#                elif key < node_key:
-#                    selected_child_attr = 'left'
-#                elif key > node_key:
-#                    selected_child_attr = 'right'
-
-#                selected_child = getattr(
-#                    current_node,
-#                    selected_child_attr
-#                )
-
-#                if selected_child is not None:
-#                    current_node = selected_child
-#                else:
-#                    break
-
-#            setattr(current_node, selected_child_attr, new_node)
-
-#    def __getitem__(self, key):
-#        current_node = self.root
-#        while current_node != None:
-#            node_key = current_node.key
-#            if key == node_key:
-#                return current_node.value
-#            elif key < node_key:
-#                current_node = current_node.left
-#            elif key > node_key:
-#                current_node = current_node.right
-
-#        raise KeyError("Key not found")
-
-#    def smaller_items(self, key):
-#        current_node = self.root
-#        def action(node, key, result):
-#            action_result = self.go_left
-#            if node.key < key:
-#                result.update(node.value)
-#                action_result |= self.go_right
-
-#            return action_result
-
-#        while current_node != None:
-#            node_key = current_node.key
-#            if key == node_key:
-#                break
-#            elif key < node_key:
-#                current_node = current_node.left
-#            elif key > node_key:
-#                current_node = current_node.right
-#        else:
-#            raise KeyError("key not found")
-
-#    def walk_tree_(self, node, action, args):
-#        if node is not None:
-#            action_result = action(node, *args)
-#            if action_result & self.go_left > 0:
-#                self.walk_tree_(node.left, action)
-#            if action_result & self.go_right > 0:
-#                self.walk_tree_(node.right, action)
-
-
-
-# class SixTreeDict:
-#    def __init__(self):
-#        pass
-
-#    def __setitem__(self, key, value):
-#        if len(key) != 6:
-#            raise ValueError("Keys must be 6 components")
